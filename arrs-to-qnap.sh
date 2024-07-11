bc5ff59dd48408a428f43dd12e6b846458812ea1
rsimmon1 <168495240+rsimmon1@users.noreply.github.com>
2024-07-10 19:12:11 -0700

    the /* on the mv outside the quotes

--- arrs-to-qnap.sh
+++ arrs-to-qnap.sh
@@ -63,7 +63,7 @@ while true; do
     
     for i in "${!MOUNTS[@]}"; do
         echo "***** ${MOUNTS[i]} *****"
-        mv "$SHARES_ROOT${SHARES[i]}/*" "$MOUNTS_ROOT${MOUNTS[i]}" || true
+        mv "$SHARES_ROOT${SHARES[i]}"/* "$MOUNTS_ROOT${MOUNTS[i]}" || true
         find "$SHARES_ROOT${SHARES[i]}" -mindepth 1 -type d -empty -delete
     done
 
